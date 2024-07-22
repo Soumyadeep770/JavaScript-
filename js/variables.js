e9a0566259f77b6174eb33c77b77d1e1dd7bf7a7
Soumyadeep770 <soumyadeepbanik746@gmail.com>
2024-07-22 17:02:58 +0000

    variables in JavaScript

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+const accountId =12456 
+/*constant value can not be charged
+*/
+let accountEmail = "SoumyaDeep@gmail.com"
+var accountPassword = 11123
+city="kolkata"
+console.log(accountId);
+accountEmail="banik@.com"
+accountPassword="222036"
+city="barasat"
+console.log(accountEmail);
+console.table([accountEmail, accountPassword, accountId ,city]);
+/*
+prefer not use var
+because of issues in bolck scope function scope
+*/
