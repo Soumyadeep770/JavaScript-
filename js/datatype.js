d4141b90b70b05f92ae1b683dcfe503121576eca
Soumyadeep770 <soumyadeepbanik746@gmail.com>
2024-07-23 06:23:02 +0000

    datatypes

---
+++ js/datatype.js
@@ -0,0 +1,11 @@
+//data type in JavaScript is Number, String, Boolean, Undefined, Null, Bigint, Symbol
+// number 2 to power 53
+// bigint
+// string =>
+// boolean true/false
+// null => standalone value
+// undefined =>
+// symbol
+//object 
+console.log(typeof null);//object 
+console.log(typeof undefined);//undefined 
