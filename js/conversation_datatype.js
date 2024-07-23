724a2868b581601b151a64582076a28ae5fd863b
Soumyadeep770 <soumyadeepbanik746@gmail.com>
2024-07-23 07:47:51 +0000

    conversation_datatype

---
+++ js/conversation_datatype.js
@@ -0,0 +1,14 @@
+//let score = ""
+
+//console.log(typeof(score));
+
+//let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIN = ""
+
+let inboolean=Boolean (isLoggedIN)
+console.log(inboolean);
+console.log(typeof inboolean);
+let strings 
